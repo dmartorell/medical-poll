8e54fd0cb770a27c953206f7833bdd270ad066a3
Daniel Martorell <dani.martorell@gmail.com>
2021-09-02 20:54:10 +0200

    Check DB to validate user code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/no-children-prop */
-import React, { FC, ReactElement, useState } from 'react';
+import React, {
+ FC, ReactElement, useState, useEffect,
+} from 'react';
 import {
   VStack, Button, Box,
 } from '@chakra-ui/react';
@@ -9,14 +11,24 @@ import GoodBye from './components/GoodBye';
 import AvatarGroup from './components/AvatarGroup';
 import UserInfoInput from './components/UserInfoInput';
 import professionals from './assets/mockData/professionals';
-
 import Survey from './components/Survey';
+import getPatientId from './helpers/getPatientId';
+
+const apiKey: any = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const apiUrl: any = import.meta.env.VITE_SUPABASE_URL;
 
 const App:FC = () => {
   const [userInfo, setUserInfo] = useState({ consent: 'true', code: '' });
+  const [patientId, setPatientId] = useState(null);
+
+  const resetData = () : void => {
+    setUserInfo({ consent: 'true', code: '' });
+    setPatientId(null);
+  };
+
   let content: ReactElement = <Welcome><UserInfoInput setUserInfo={setUserInfo} /></Welcome>;
 
-  if (userInfo.consent === 'false') {
+  if (userInfo.consent === 'false' && patientId) {
     content = (
       <>
         <GoodBye>
@@ -25,7 +37,7 @@ const App:FC = () => {
         <Box p={5}>
           <Button
             w="auto"
-            onClick={() => setUserInfo({ consent: 'true', code: '' })}
+            onClick={resetData}
             size="md"
             colorScheme="blue"
             variant="solid"
@@ -38,11 +50,29 @@ const App:FC = () => {
     );
   }
 
-  if (userInfo.consent === 'true' && userInfo.code) {
+  if (userInfo.consent === 'true' && patientId) {
     content = (
       <Survey patientId={patientId} />
     );
   }
+
+  useEffect(() => {
+    if (userInfo.code) {
+      try {
+        fetch(`${apiUrl}/patient?id=eq.${getPatientId(userInfo.code)}`, { headers: { apiKey } })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.length) {
+             setPatientId(data[0].id);
+            } else {
+              console.log('PATIENTE INEXISTENTE');
+            }
+          });
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }, [userInfo]);
 
   return (
     <Box backgroundColor="twitter.50" height="100vh" py={{ sm: 0, lg: 10 }}>
