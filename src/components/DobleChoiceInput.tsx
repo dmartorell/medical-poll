c28b60d712657ce830e572438fab2f4e88072754
Daniel Martorell <dani.martorell@gmail.com>
2021-09-07 10:30:10 +0200

    Fix isRequired in DobleChoice component

--- src/components/DobleChoiceInput.tsx
+++ src/components/DobleChoiceInput.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 import React, { FC, useState, useEffect } from 'react';
 import {
- Radio, RadioGroup, Box, FormControl, Stack, Text,
+ Radio, RadioGroup, Box, FormControl, VStack, Text, HStack,
 } from '@chakra-ui/react';
 
 type Props = {
@@ -19,17 +19,17 @@ type Props = {
 };
 
 const DobleChoiceInput: FC<Props> = ({
- questionId, question, choices, setNewAnswers, rows, project, patientId,
+ question, questionId, choices, setNewAnswers, rows, project, patientId,
 }) => {
   const [frecuencia, setFrecuencia] = useState('');
-    const [gravedad, setGravedad] = useState('');
+  const [gravedad, setGravedad] = useState('');
 
   const { choices1, choices2 } = choices;
 
   const changeHandler = ({ target }: any) => {
     const { value } = target;
     const section = target.name;
-    if (section === 'frecuencia') {
+    if (section.includes('frecuencia')) {
         setFrecuencia(value);
     } else {
         setGravedad(value);
@@ -50,47 +50,96 @@ useEffect(() => {
   }
 }, [frecuencia, gravedad]);
 
-  return (
-
-    <Box borderWidth="1px" boxShadow="sm" borderColor="grey.50" borderRadius="lg" p={5} w="100%" bg="rgba(123, 120, 120, 0.01)" alignSelf="flex-start">
-      <Text mb={6} w="100%" bg="blue.800" px={4} py={2} borderRadius="md" color="white" fontSize={{ sm: 14, lg: 17 }}>
+return (
+  <>
+    <Box
+      borderWidth="1px"
+      boxShadow="sm"
+      borderColor="grey.50"
+      borderRadius="lg"
+      p={5}
+      w="100%"
+      bg="rgba(123, 120, 120, 0.01)"
+    >
+      <Text
+        mb={6}
+        bg="blue.800"
+        px={4}
+        py={2}
+        borderRadius="md"
+        color="white"
+        fontSize={{ sm: 14, lg: 17 }}
+      >
         {question}
       </Text>
-      <Stack direction="row" justifyContent="center" spacing="100px">
-        <FormControl isRequired>
-          <RadioGroup colorScheme="blue" size="md">
-            <Text mb={3} color="gray.500" fontSize="md">FRECUENCIA</Text>
-            <Stack direction="column" spacing={1}>
-              {
-              choices1?.map(
-              (choice, index) => {
-                  const questionValue: number = index;
-                  return <Radio alignSelf="flex-start" key={choice} name="frecuencia" value={`${questionValue}`} onChange={changeHandler}>{choice}</Radio>;
-              },
-              )
-          }
-            </Stack>
-          </RadioGroup>
-        </FormControl>
-        <FormControl isRequired>
-          <RadioGroup colorScheme="blue" size="md">
-            <Text mb={3} color="gray.500" fontSize="md">GRAVEDAD</Text>
-            <Stack direction="column" spacing={1}>
-              {
-              choices2?.map(
-              (choice, index) => {
-                  const questionValue: number = index;
-                  return <Radio alignSelf="flex-start" key={choice} name="gravedad" value={`${questionValue}`} onChange={changeHandler}>{choice}</Radio>;
-              },
-              )
-          }
-            </Stack>
-          </RadioGroup>
-        </FormControl>
-      </Stack>
+      <HStack justifyContent="center" spacing="100px">
+        <HStack>
+          <FormControl isRequired>
+            <Text
+              mb={3}
+              color="blue.800"
+              fontWeight="thin"
+              fontSize={{ sm: 14, lg: 17 }}
+            >
+              FRECUENCIA
+            </Text>
+            <RadioGroup colorScheme="blue" size="md">
+              <VStack spacing={1}>
+                {choices1?.map((choice, index) => {
+                const questionValue = index;
+                return (
+                  <Radio
+                    alignSelf="flex-start"
+                    key={choice}
+                    value={`${questionValue}`}
+                    onChange={changeHandler}
+                    name={`${questionId}frecuencia`}
+                  >
+                    {choice}
+                  </Radio>
+                );
+              })}
+              </VStack>
+            </RadioGroup>
+          </FormControl>
+        </HStack>
+        <HStack>
+          <FormControl isRequired>
+            <Text
+              mb={3}
+              color="blue.800"
+              fontWeight="thin"
+              fontSize={{ sm: 14, lg: 17 }}
+            >
+              GRAVEDAD
+            </Text>
+            <RadioGroup colorScheme="blue" size="md">
+              <VStack spacing={1}>
+                {choices2?.map((choice, index) => {
+                const questionValue = index;
+                return (
+                  <Radio
+                    alignSelf="flex-start"
+                    key={choice}
+                    value={`${questionValue}`}
+                    onChange={changeHandler}
+                    name={`${questionId}gravedad`}
+
+                  >
+                    {choice}
+                  </Radio>
+                );
+              })}
+              </VStack>
+            </RadioGroup>
+          </FormControl>
+        </HStack>
+      </HStack>
     </Box>
 
-  );
+  </>
+
+);
  };
 
 export default DobleChoiceInput;
